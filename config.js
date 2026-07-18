3b0022506c434820ce3588a0a8216ec6cf440705
Rob Garrison <153874262+Rwg-cmd@users.noreply.github.com>
2026-07-18 19:15:47 -0400

    Update SUPABASE_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WV_CONFIG = {
-  SUPABASE_URL: 'https://xlbziukgeulhiowushnpu.supabase.co'',
+  SUPABASE_URL: 'https://xlbziukgeulhiowusnpu.supabase.co/rest/v1/'',
   SUPABASE_KEY: 'sb_publishable_DmlnTDmH-6gjpw9ieLZorg_s7bACSa_'
 };
