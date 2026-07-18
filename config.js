12609fc06374c05c455543dbf1cede9187b1e4ac
Rob Garrison <153874262+Rwg-cmd@users.noreply.github.com>
2026-07-18 19:08:05 -0400

    Update SUPABASE_URL to remove trailing path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.WV_CONFIG = {
-  SUPABASE_URL: 'https://xlbziukgeulhiowushnpu.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://xlbziukgeulhiowushnpu.supabase.co',
   SUPABASE_KEY: 'sb_publishable_DmlnTDmH-6gjpw9ieLZorg_s7bACSa_'
 };
